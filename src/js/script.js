efb0af4cff8f6255200aacc24a719eff150fbbe4
Alexandr <manlain1231@gmail.com>
2023-03-04 15:07:50 +0300

    fifth and sixth section

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@
     autoplay: false,
     controls: false,
     nav: false,
+    speed: 1200,
     responsive: {
         320: {
           edgePadding: 20,
@@ -30,3 +31,28 @@
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   }); 
+
+
+  $(function() {
+      
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $('.catalog-item__content').addClass('catalog-item__content_active');
+      $('.catalog-item__list').removeClass('catalog-item__list_active');
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+  });
