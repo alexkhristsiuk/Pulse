478d605b76b1c5881aa5e2093ec838246d9d460a
Alexandr <manlain1231@gmail.com>
2023-03-07 10:23:36 +0300

    modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -56,3 +56,65 @@
     toggleSlide('.catalog-item__back');
 
   });
+
+  //Modal
+
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+ 
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+function validateForms(form){
+  $(form).validate({
+    rules: {
+      name: "required",
+      phone: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: "Пожалуйста, введите свое имя",
+      phone: "Пожалуйста, введите свой номер телефона",
+      email: {
+        required: "Пожалуйста, введите свою почту",
+        email: "Неправильно введен адрес почты"
+      }
+    }
+  });
+};
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone').mask("+7 (999) 999-9999");
+
+$('form').submit(function(e) {
+  e.preventDefault();
+
+  if (!$(this).valid()) {
+    return;
+  }
+
+  $ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize()
+  }).done(function() {
+      $(this).find("input").val("");
+
+
+      $('form').trigger('reset');
+  });
+  return false;
+});
